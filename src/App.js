02e6aa57d780f7d77f5379637a538f8343ff2659
mehmetak <mehmetak78@hotmail.com>
2019-07-15 14:10:47 +0300

    Context & Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 
-import React, {Fragment} from 'react';
+import React from 'react';
 import Navbar from "./components/layout/Navbar"
-import Users from "./components/users/Users";
 
-import Search from "./components/users/Search";
+
+
 import Alert from "./components/layout/Alert";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import About from "./components/pages/About";
@@ -12,6 +12,8 @@ import User from "./components/users/User";
 
 import GitHubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 
 const App = () => {
@@ -41,21 +43,12 @@ const App = () => {
                         <div className="container">
                             <Alert />
                             <Switch>
-                                <Route exact path="/" render={props => (
-                                    <Fragment>
-                                        <Search/>
-                                        <Users/>
-                                    </Fragment>
-                                    )}>
-                                </Route>
+                                <Route exact path="/" component={Home}/>
                                 <Route exact path={"/about"} component={About} />
                                 <Route exact path={"/user/:login"} component={User} />
-                                )} />
-
+                                <Route component={NotFound}/>
                             </Switch>
-
                         </div>
-
                     </div>
                 </Router>
             </AlertState>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NotFound = (props) => {
+    return (
+        <div>
+            <h1>NotFound</h1>
+            <p className="lead">The page you are looking for doesn't exist...</p>
+        </div>
+    );
+};
+
+
+export default NotFound;
